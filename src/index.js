4397be3a7644f66925075f850987b7acb7003229
Dor Mesica <dor.mesica@autodesk.com>
2020-12-27 12:42:28 +0200

    Added isAlive entrypoint.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+app.get('/isAlive', (req, res) => {
+    res.send("Alive and kickin'!!")
+});
+
 io.on('connection', socket => {
     console.log('a user has connected');
 
