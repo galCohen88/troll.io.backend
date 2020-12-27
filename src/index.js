169aa0db5fee7a736687948ef571888f31458cd1
Dor Mesica <dor.mesica@autodesk.com>
2020-12-27 14:53:44 +0200

    Added message to new client.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const loginRouter = require('./api/login');
+
+
+const PORT = 80 || process.env.PORT;
+
 app.get('/isAlive', (req, res) => {
     res.send("Alive and kickin'!!")
 });
 
 io.on('connection', socket => {
     console.log('a user has connected');
+    socket.send('Howdy!');
 
     socket.on('disconnect', () => {
         console.log('a user has disconnected');
     });
 });
 
-http.listen(80, () => {
-    console.log('Troll.io server listening on port 80');
+http.listen(PORT, () => {
+    console.log(`Troll.io server listening on port ${PORT}`);
 });
