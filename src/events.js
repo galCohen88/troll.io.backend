4f9c434dc9fb04600ca425c70b45db68e5114ad6
Dor Mesica <dor.mesica@autodesk.com>
2020-12-28 12:24:04 +0200

    Fixed payload. Added handle troll event.

--- src/events.js
+++ src/events.js
@@ -1,14 +1,21 @@
 const SocketCollection = require('./SocketCollection');
+const handleTroll = require('./handlers/trolling');
 
 function registerHandlers(socket) {
-    socket.on('login', ({ username }) => {
+    socket.on('login', payload => {
+        const { username } = JSON.parse(payload);
+
         SocketCollection.registerUser(username, socket);
         console.log(`${username} is now connected`);
     });
     socket.on('logoff', ({ username }) => {
+        const { username } = JSON.parse(payload);
+
         SocketCollection.remove(username);
         console.log(`${username} has disconnected`);
     });
+
+    socket.on('troll', handleTroll);
 
     socket.on('disconnect', () => {
         console.log('a user has disconnected');
---
+++ src/handlers/trolling.js
@@ -0,0 +1,11 @@
+
+function handleTroll({
+    destination,
+    media,
+    email,
+    sms
+}) {
+    
+}
+
+module.exports = handleTroll;
